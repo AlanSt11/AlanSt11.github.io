9628fc2104a1737fce83203fe39fa4b663f0ec5f
AlanSt11 <86734556+AlanSt11@users.noreply.github.com>
2022-07-10 18:38:41 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2000,
+    delay: 400
+});
+
+ScrollReveal().reveal('.box__service', { delay: 500,  origin: 'top' });
+ScrollReveal().reveal('.caja__contacto', { delay: 200,  origin: 'bottom' });
