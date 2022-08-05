159595e40a7e614dca1640eb8c5237d19a5f9e1b
AlanSt11 <86734556+AlanSt11@users.noreply.github.com>
2022-08-05 16:26:46 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-ScrollReveal({
-    reset: true,
-    distance: '60px',
-    duration: 2000,
-    delay: 400
-});
-
-ScrollReveal().reveal('.box__service', { delay: 500,  origin: 'top' });
-ScrollReveal().reveal('.caja__contacto', { delay: 200,  origin: 'bottom' });
