900b5d62a3d6e83e7fa33e39919bda234f220495
AlanSt11 <86734556+AlanSt11@users.noreply.github.com>
2022-08-24 13:54:29 -0500

    Add files via upload

---
+++ AlanSt11.github.io-main/js/main.js
@@ -0,0 +1,60 @@
+/* =======================================
+             MENU RESPONSIVE
+   =======================================
+*/
+
+let menu = document.getElementById('sticky');
+let btnshare = document.querySelector('.share');
+let options = document.querySelector('.options')
+
+window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+    if(scroll > 50){
+        menu.classList.add("sticky-show");
+        btnshare.style.display = "none";
+        options.style.justifyContent = "center";
+    } else if(scroll < 50){
+        menu.classList.remove("sticky-show");
+        btnshare.style.display = "flex";
+        options.style.justifyContent = "flex-end"
+    }
+
+}
+
+/* =======================================
+            OPCION DE COMPARTIR
+   =======================================
+*/
+function share (){
+    if (navigator.share) {
+        navigator.share({
+            title: 'Alan St',
+            text: 'Encuentrame en todas las redes sociales',
+            url: ''
+        });
+        console.log("share");
+    } else {
+        navigator.clipboard.writeText('Link copeado correctamente');
+    }
+}
+
+/* =======================================
+           EFECTO MAQUINA DE ESCRIBIR              
+    =======================================
+*/
+
+const typed = new Typed('.typed', {
+    strings: ['<i class="title">emprendedor</i>', '<i class="title">programador</i>'],
+
+    color: '#000',
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html',
+})
